ca6f42e73674c0d3f7a0eb3cee84cf1113e804a2
Eric <ewwang@umail.ucsb.edu>
2023-12-04 00:10:11 -0500

    add data for testing

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,8 @@
 ## code to prepare `DATASET` dataset goes here
 
+studies_subset = readRDS("studies_subset.rds")
+designs = readRDS("designs.rds")
 accel = readRDS("accel.rds")
 usethis::use_data(accel, overwrite = TRUE)
+usethis::use_data(designs, overwrite = TRUE)
+usethis::use_data(studies_subset, overwrite = TRUE)

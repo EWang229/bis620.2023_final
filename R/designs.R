1a53712b21de8a1a20b2b59c1f3d9fc90b0ecdfc
Eric <ewwang@umail.ucsb.edu>
2023-12-04 00:30:48 -0500

    update functions

---
+++ R/designs.R
@@ -0,0 +1,10 @@
+#' Designs Data from ctrialsgov
+#'
+#'Designs data for BIS620
+#'
+#' @format ## `designs`
+#' A data frame with 465,815 rows and 14 columns:
+#' \describe{
+#'   ...
+#' }
+"designs"
---
+++ R/studies_subset.R
@@ -0,0 +1,10 @@
+#' A Subset of Data from the Studies Dataset from ctrialsgov
+#'
+#'Subsetted data for BIS620
+#'
+#' @format ## `studies_subset`
+#' A data frame with 1,000 rows and 70 columns:
+#' \describe{
+#'   ...
+#' }
+"studies_subset"

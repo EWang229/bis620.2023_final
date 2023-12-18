5e90ef2ce7aec456cca5b5f5dd19630a426298f5
Eric <ewwang@umail.ucsb.edu>
2023-12-18 03:45:06 -0500

    update functions for final

--- R/aov_results.R
+++ R/aov_results.R
@@ -7,16 +7,16 @@
 #' @export
 aov_results = function(x){
   x <- aov(glyhb ~ location*frame, data = x)
-  dust(x) %>%  # formats the anova object
-    sprinkle(col = 2:5, round = 3) %>%  # rounds to the 3rd decimal place for
+  dust(x) |>  # formats the anova object
+    sprinkle(col = 2:5, round = 3) |>  # rounds to the 3rd decimal place for
     # columns 2-5
-    sprinkle(col = 6, fn = quote(pvalString(value))) %>%  # make the p-value
+    sprinkle(col = 6, fn = quote(pvalString(value))) |>  # make the p-value
     # not in scientific notation
     sprinkle_colnames(df = "Degrees of Freedom",
                       sumsq = "Sum of Squares",
                       meansq = "Mean Squared Error",
                       statistic = "F-value",
-                      p.value = "p-value") %>%  # re-naming the columns
-    kable() %>%  # this makes the table
+                      p.value = "p-value") |>  # re-naming the columns
+    kable() |>  # this makes the table
     kable_styling()  # changes the style of the table
 }
--- R/lr_results.R
+++ R/lr_results.R
@@ -10,15 +10,15 @@
 lr_results = function(x){
   log_glyhb <- log(x$glyhb)
   x <- lm(log_glyhb ~ ratio, data = x)
-  dust(x) %>%   # formats the lm object
-    sprinkle(col = 2:4, round = 3) %>%  # rounds columns 2-4 to 3 decimals
-    sprinkle(col = 5, fn = quote(pvalString(value))) %>%  # takes away
+  dust(x) |>   # formats the lm object
+    sprinkle(col = 2:4, round = 3) |>  # rounds columns 2-4 to 3 decimals
+    sprinkle(col = 5, fn = quote(pvalString(value))) |>  # takes away
     # scientific notation for p-value
     sprinkle_colnames(term = "Term",
                       estimate = "Estimate",
                       std.error = "SE",
                       statistic = "T-statistic",
-                      p.value = "p-value") %>%  # renaming the columns
-    kable() %>%  # makes the table
+                      p.value = "p-value") |>  # renaming the columns
+    kable() |>  # makes the table
     kable_styling()  # changes the style of the table
 }

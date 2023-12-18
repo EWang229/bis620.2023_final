6a02f58dc71c53db85f964bbec61a0ea95110438
Eric <ewwang@umail.ucsb.edu>
2023-12-18 02:31:46 -0500

    update functions for final

--- R/lr_assumptions.R
+++ R/lr_assumptions.R
@@ -2,6 +2,7 @@
 #'
 #' @param x a dataframe to check linear regression assumptions on (for the
 #' ratio of cholesterol to high-density lipoproteins)
+#' @param log a logical that asks whether to log the response variable
 #' @returns 4 plots that allow us to diagnose linear regression assumptions
 #' @export
 lr_assumptions = function(x, log = FALSE){

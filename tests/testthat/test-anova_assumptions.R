b5b9cb981a7f80e7663619e2d48db74e69fee5a5
Eric <ewwang@umail.ucsb.edu>
2023-12-18 02:11:42 -0500

    update tests

--- tests/testthat/test-anova_assumptions.R
+++ tests/testthat/test-anova_assumptions.R
@@ -1,5 +1,4 @@
 test_that("anova_assumptions works", {
   data(diabetes)
-  par(mfrow = c(2, 2))
   vdiffr::expect_doppelganger("anova_plots1", anova_assumptions(diabetes))
 })
--- tests/testthat/test-lr_assumptions.R
+++ tests/testthat/test-lr_assumptions.R
@@ -1,5 +1,4 @@
 test_that("lr_assumptions works", {
   data(diabetes)
-  par(mfrow = c(2, 2))
   vdiffr::expect_doppelganger("lr_plots1", lr_assumptions(diabetes))
 })
